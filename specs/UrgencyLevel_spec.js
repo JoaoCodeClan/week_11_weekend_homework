0863bedf01a12378ee58a408c252480445bb144e
Joao <johnynecas@hotmail.com>
2017-12-08 18:48:13 +0000

    created test for urgency level emergency

--- specs/UrgencyLevel_spec.js
+++ specs/UrgencyLevel_spec.js
@@ -27,6 +27,17 @@ let foryest;
    assert.strictEqual(1,urg);
 
  })
+ it("should be able to get a value  of 2 for emergency", function(){
+
+   assert.strictEqual(2,emer);
+
+ })
+
+ it("should be able to get a value  of 3 for Foryesterday", function(){
+
+   assert.strictEqual(3,foryest);
+
+ })
 
 
 })
