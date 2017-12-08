5708a2896c68589d9b4295d7750d2a053a727f48
Joao <johnynecas@hotmail.com>
2017-12-08 11:15:21 +0000

    crwated cosnstructor for food

---
+++ food.js
@@ -0,0 +1,9 @@
+const _=require('lodash');
+
+
+const Food = function(name, replenishment,isPoisoned){
+  this.name= name;
+  this.replenishment = replenishment;
+  this.isPoisoned = isPoisoned;
+
+})
