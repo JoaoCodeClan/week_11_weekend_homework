127fa690ab1cabfabea9957ecab5b66e0d507c81
Joao <johnynecas@hotmail.com>
2017-12-08 18:36:11 +0000

    tested that can get diff levle for very hard

--- specs/difficultyLevel_spec.js
+++ specs/difficultyLevel_spec.js
@@ -17,20 +17,25 @@ let dif3;
  });
 
 
- it("should be able to get a value for difficulty level easy", function(){
+ it("should be able to get a value  of 0 for difficulty level easy", function(){
 
    assert.strictEqual(0,dif);
 
  })
- it("should be able to get a value for difficulty level medium", function(){
+ it("should be able to get a value of 1 for difficulty level medium", function(){
 
    assert.strictEqual(1,dif1);
 
  })
 
- it("should be able to get a value for difficulty level hard", function(){
+ it("should be able to get a value of 2 for difficulty level hard", function(){
 
    assert.strictEqual(2,dif2);
+
+ })
+ it("should be able to get a value of 3 for difficulty very hard", function(){
+
+   assert.strictEqual(3,dif3);
 
  })
 })
