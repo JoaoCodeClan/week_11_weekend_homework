288d62ee8d4fc0534d0056f6c4a88ca2ed1ee795
Joao <johnynecas@hotmail.com>
2017-12-08 18:33:04 +0000

    tested that can get diff levle for easy

--- specs/difficultyLevel_spec.js
+++ specs/difficultyLevel_spec.js
@@ -1,23 +1,25 @@
-// const DifficultyLevel = require("../difficultyLevel.js");
-//
-// describe("DifficultyLevel", function(){
-//
-// let dif;
-// let dif1;
-// let dif2;
-// let dif3;
-//
-//  beforeEach(function(){
-//    dif= DifficultyLevel.Easy;
-//    dif1= DifficultyLevel.Medium;
-//    dif2= DifficultyLevel.Hard;
-//    dif3 = DifficultyLevel.Very_Hard;
-//
-//  });
-//
-//
-//  it("should be able to get a value for difficulty level", function(){
-//
-//    assert.strictEqual(0,dif.);
-//
-//  })
+const DifficultyLevel = require("../difficultyLevel.js");
+const assert = require("assert");
+
+describe("DifficultyLevel", function(){
+
+let dif;
+let dif1;
+let dif2;
+let dif3;
+
+ beforeEach(function(){
+   dif= DifficultyLevel.Easy;
+   dif1= DifficultyLevel.Medium;
+   dif2= DifficultyLevel.Hard;
+   dif3 = DifficultyLevel.Very_Hard;
+
+ });
+
+
+ it("should be able to get a value for difficulty level", function(){
+
+   assert.strictEqual(0,dif);
+
+ })
+})
