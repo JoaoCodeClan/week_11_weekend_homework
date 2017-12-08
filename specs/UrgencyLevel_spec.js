f1a3c2b74cb52694efffea9fb0e5efac19aaeac1
Joao <johnynecas@hotmail.com>
2017-12-08 18:46:06 +0000

    created test for urgency levle not urgent

---
+++ specs/UrgencyLevel_spec.js
@@ -0,0 +1,28 @@
+const UrgencyLevel = require("../urgencyLevel.js");
+const assert = require("assert");
+
+describe("UrgencyLevel", function(){
+
+let noturg;
+let urg;
+let emer;
+let foryest;
+
+ beforeEach(function(){
+   noturg= UrgencyLevel.NotUrgent;
+   urg= UrgencyLevel.Urgent;
+   emer= UrgencyLevel.Emergency;
+   foryest = UrgencyLevel.ForYesterday;
+
+ });
+
+
+ it("should be able to get a value  of 0 for not urgent", function(){
+
+   assert.strictEqual(0,noturg);
+
+ })
+
+
+
+})
