e33cca33406d7880a2e258e2533bf2aea561798f
Joao <johnynecas@hotmail.com>
2017-12-08 11:33:02 +0000

    created and tested getter for food replenishement"

--- specs/food_test.js
+++ specs/food_test.js
@@ -15,5 +15,12 @@ it("food should have a name", function(){
   assert.strictEqual("pasta", food.name);
 })
 
+it("food should have a replenishment value", function(){
+  assert.strictEqual(10, food.replenishment);
+})
+
+it("food should be able to check if its poisoned", function(){
+  assert.strictEqual(10, food.replenishment);
+})
 
 })
